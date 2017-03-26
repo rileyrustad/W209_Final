ee41eabefde5eab736c6aba4ed1e558ac55016ae
Riley Rustad <rileyrustad@gmail.com>
2017-03-25 18:37:29 -0700

    stoped using d3, ported everything to leaflet. Now need to implement slider custom controls

---
+++ data/demographics.js
@@ -0,0 +1,93 @@
+var portlandDemo = [
+ {
+   "demographic": "Avg. Portland Household",
+   "income": 53230,
+   "rent": 1330.75,
+   "Household composition": "2.08",
+   "Households with Childre": 0.25
+ },
+ {
+   "demographic": "3-Person Extremely Low-Income",
+   "income": 19800,
+   "rent": 495,
+   "Household composition": "-",
+   "Households with Childre": 0.19
+ },
+ {
+   "demographic": "3-Person Low-Income",
+   "income": 39600,
+   "rent": 990,
+   "Household composition": "-",
+   "Households with Childre": 0.2
+ },
+ {
+   "demographic": "3-Person Moderate-Income",
+   "income": 52800,
+   "rent": 1320,
+   "Household composition": "-",
+   "Households with Childre": 0.16
+ },
+ {
+   "demographic": "Couple with Famly ",
+   "income": 84114,
+   "rent": 2102.85,
+   "Household composition": "4.04",
+   "Households with Childre": 100
+ },
+ {
+   "demographic": "White",
+   "income": 57006,
+   "rent": 1425.15,
+   "Household composition": "2.02",
+   "Households with Childre": 0.24
+ },
+ {
+   "demographic": "Black ",
+   "income": 27441,
+   "rent": 686.025,
+   "Household composition": "1.84",
+   "Households with Childre": 0.37
+ },
+ {
+   "demographic": "Latino",
+   "income": 36312,
+   "rent": 907.8,
+   "Household composition": "2.4",
+   "Households with Childre": 0.37
+ },
+ {
+   "demographic": "Native American",
+   "income": 27134,
+   "rent": 678.35,
+   "Household composition": "2.6",
+   "Households with Childre": 0.37
+ },
+ {
+   "demographic": "Asian",
+   "income": 53377,
+   "rent": 1334.425,
+   "Household composition": "2.73",
+   "Households with Childre": 0.24
+ },
+ {
+   "demographic": "Senior",
+   "income": 37469,
+   "rent": 936.725,
+   "Household composition": "1.62",
+   "Households with Childre": 0.03
+ },
+ {
+   "demographic": "Single Mother",
+   "income": 31614,
+   "rent": 790.35,
+   "Household composition": "3.33",
+   "Households with Childre": 100
+ },
+ {
+   "demographic": "Foreign-Born",
+   "income": 41340,
+   "rent": 1033.5,
+   "Household composition": "3.01",
+   "Households with Childre": 0.38
+ }
+]
---
+++ data/rent.js
@@ -0,0 +1,602 @@
+var portlandRent = [
+ {
+   "neighborhood": "122-Division",
+   "housing size": "Studio ",
+   "rent": "677"
+ },
+ {
+   "neighborhood": "122-Division",
+   "housing size": "1-BR",
+   "rent": "814"
+ },
+ {
+   "neighborhood": "122-Division",
+   "housing size": "2-BR",
+   "rent": "955"
+ },
+ {
+   "neighborhood": "122-Division",
+   "housing size": "3-BR",
+   "rent": "1234"
+ },
+ {
+   "neighborhood": "122-Division",
+   "housing size": "Homeownership",
+   "rent": "951"
+ },
+ {
+   "neighborhood": "Belmont-Hawthorne-Division",
+   "housing size": "Studio ",
+   "rent": "1017"
+ },
+ {
+   "neighborhood": "Belmont-Hawthorne-Division",
+   "housing size": "1-BR",
+   "rent": "1278"
+ },
+ {
+   "neighborhood": "Belmont-Hawthorne-Division",
+   "housing size": "2-BR",
+   "rent": "1410"
+ },
+ {
+   "neighborhood": "Belmont-Hawthorne-Division",
+   "housing size": "3-BR",
+   "rent": "1853"
+ },
+ {
+   "neighborhood": "Belmont-Hawthorne-Division",
+   "housing size": "Homeownership",
+   "rent": "2109"
+ },
+ {
+   "neighborhood": "Centennial-Glenfair-Wilkes",
+   "housing size": "Studio ",
+   "rent": "791"
+ },
+ {
+   "neighborhood": "Centennial-Glenfair-Wilkes",
+   "housing size": "1-BR",
+   "rent": "778"
+ },
+ {
+   "neighborhood": "Centennial-Glenfair-Wilkes",
+   "housing size": "2-BR",
+   "rent": "927"
+ },
+ {
+   "neighborhood": "Centennial-Glenfair-Wilkes",
+   "housing size": "3-BR",
+   "rent": "1148"
+ },
+ {
+   "neighborhood": "Centennial-Glenfair-Wilkes",
+   "housing size": "Homeownership",
+   "rent": "951"
+ },
+ {
+   "neighborhood": "Central City",
+   "housing size": "Studio ",
+   "rent": "1132"
+ },
+ {
+   "neighborhood": "Central City",
+   "housing size": "1-BR",
+   "rent": "1481"
+ },
+ {
+   "neighborhood": "Central City",
+   "housing size": "2-BR",
+   "rent": "2125"
+ },
+ {
+   "neighborhood": "Central City",
+   "housing size": "3-BR",
+   "rent": "3308"
+ },
+ {
+   "neighborhood": "Central City",
+   "housing size": "Homeownership",
+   "rent": "1762"
+ },
+ {
+   "neighborhood": "Forest Park-Northwest Hills",
+   "housing size": "Studio ",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Forest Park-Northwest Hills",
+   "housing size": "1-BR",
+   "rent": "1386"
+ },
+ {
+   "neighborhood": "Forest Park-Northwest Hills",
+   "housing size": "2-BR",
+   "rent": "1542"
+ },
+ {
+   "neighborhood": "Forest Park-Northwest Hills",
+   "housing size": "3-BR",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Forest Park-Northwest Hills",
+   "housing size": "Homeownership",
+   "rent": "2852"
+ },
+ {
+   "neighborhood": "Gateway",
+   "housing size": "Studio ",
+   "rent": "474"
+ },
+ {
+   "neighborhood": "Gateway",
+   "housing size": "1-BR",
+   "rent": "845"
+ },
+ {
+   "neighborhood": "Gateway",
+   "housing size": "2-BR",
+   "rent": "1050"
+ },
+ {
+   "neighborhood": "Gateway",
+   "housing size": "3-BR",
+   "rent": "1398"
+ },
+ {
+   "neighborhood": "Gateway",
+   "housing size": "Homeownership",
+   "rent": "1105"
+ },
+ {
+   "neighborhood": "Hayden Island",
+   "housing size": "Studio ",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Hayden Island",
+   "housing size": "1-BR",
+   "rent": "1586"
+ },
+ {
+   "neighborhood": "Hayden Island",
+   "housing size": "2-BR",
+   "rent": "2334"
+ },
+ {
+   "neighborhood": "Hayden Island",
+   "housing size": "3-BR",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Hayden Island",
+   "housing size": "Homeownership",
+   "rent": "1221"
+ },
+ {
+   "neighborhood": "Hillsdale-Multnomah-Barbur",
+   "housing size": "Studio ",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Hillsdale-Multnomah-Barbur",
+   "housing size": "1-BR",
+   "rent": "909"
+ },
+ {
+   "neighborhood": "Hillsdale-Multnomah-Barbur",
+   "housing size": "2-BR",
+   "rent": "1097"
+ },
+ {
+   "neighborhood": "Hillsdale-Multnomah-Barbur",
+   "housing size": "3-BR",
+   "rent": "1498"
+ },
+ {
+   "neighborhood": "Hillsdale-Multnomah-Barbur",
+   "housing size": "Homeownership",
+   "rent": "1812"
+ },
+ {
+   "neighborhood": "Hollywood",
+   "housing size": "Studio ",
+   "rent": "1071"
+ },
+ {
+   "neighborhood": "Hollywood",
+   "housing size": "1-BR",
+   "rent": "1240"
+ },
+ {
+   "neighborhood": "Hollywood",
+   "housing size": "2-BR",
+   "rent": "1558"
+ },
+ {
+   "neighborhood": "Hollywood",
+   "housing size": "3-BR",
+   "rent": "2102"
+ },
+ {
+   "neighborhood": "Hollywood",
+   "housing size": "Homeownership",
+   "rent": "2703"
+ },
+ {
+   "neighborhood": "Interstate Corridor",
+   "housing size": "Studio ",
+   "rent": "1084"
+ },
+ {
+   "neighborhood": "Interstate Corridor",
+   "housing size": "1-BR",
+   "rent": "1328"
+ },
+ {
+   "neighborhood": "Interstate Corridor",
+   "housing size": "2-BR",
+   "rent": "1762"
+ },
+ {
+   "neighborhood": "Interstate Corridor",
+   "housing size": "3-BR",
+   "rent": "1123"
+ },
+ {
+   "neighborhood": "Interstate Corridor",
+   "housing size": "Homeownership",
+   "rent": "1767"
+ },
+ {
+   "neighborhood": "Lents-Foster",
+   "housing size": "Studio ",
+   "rent": "950"
+ },
+ {
+   "neighborhood": "Lents-Foster",
+   "housing size": "1-BR",
+   "rent": "897"
+ },
+ {
+   "neighborhood": "Lents-Foster",
+   "housing size": "2-BR",
+   "rent": "1070"
+ },
+ {
+   "neighborhood": "Lents-Foster",
+   "housing size": "3-BR",
+   "rent": "1344"
+ },
+ {
+   "neighborhood": "Lents-Foster",
+   "housing size": "Homeownership",
+   "rent": "1137"
+ },
+ {
+   "neighborhood": "MLK-Alberta",
+   "housing size": "Studio ",
+   "rent": "1207"
+ },
+ {
+   "neighborhood": "MLK-Alberta",
+   "housing size": "1-BR",
+   "rent": "1116"
+ },
+ {
+   "neighborhood": "MLK-Alberta",
+   "housing size": "2-BR",
+   "rent": "1436"
+ },
+ {
+   "neighborhood": "MLK-Alberta",
+   "housing size": "3-BR",
+   "rent": "876"
+ },
+ {
+   "neighborhood": "MLK-Alberta",
+   "housing size": "Homeownership",
+   "rent": "1956"
+ },
+ {
+   "neighborhood": "Montavilla",
+   "housing size": "Studio ",
+   "rent": "930"
+ },
+ {
+   "neighborhood": "Montavilla",
+   "housing size": "1-BR",
+   "rent": "893"
+ },
+ {
+   "neighborhood": "Montavilla",
+   "housing size": "2-BR",
+   "rent": "1133"
+ },
+ {
+   "neighborhood": "Montavilla",
+   "housing size": "3-BR",
+   "rent": "1339"
+ },
+ {
+   "neighborhood": "Montavilla",
+   "housing size": "Homeownership",
+   "rent": "1472"
+ },
+ {
+   "neighborhood": "Northwest",
+   "housing size": "Studio ",
+   "rent": "1095"
+ },
+ {
+   "neighborhood": "Northwest",
+   "housing size": "1-BR",
+   "rent": "1618"
+ },
+ {
+   "neighborhood": "Northwest",
+   "housing size": "2-BR",
+   "rent": "2585"
+ },
+ {
+   "neighborhood": "Northwest",
+   "housing size": "3-BR",
+   "rent": "2286"
+ },
+ {
+   "neighborhood": "Northwest",
+   "housing size": "Homeownership",
+   "rent": "2084"
+ },
+ {
+   "neighborhood": "Parkrose-Argay",
+   "housing size": "Studio ",
+   "rent": "473"
+ },
+ {
+   "neighborhood": "Parkrose-Argay",
+   "housing size": "1-BR",
+   "rent": "872"
+ },
+ {
+   "neighborhood": "Parkrose-Argay",
+   "housing size": "2-BR",
+   "rent": "1103"
+ },
+ {
+   "neighborhood": "Parkrose-Argay",
+   "housing size": "3-BR",
+   "rent": "1178"
+ },
+ {
+   "neighborhood": "Parkrose-Argay",
+   "housing size": "Homeownership",
+   "rent": "1132"
+ },
+ {
+   "neighborhood": "Pleasant Valley ",
+   "housing size": "Studio ",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Pleasant Valley ",
+   "housing size": "1-BR",
+   "rent": "815"
+ },
+ {
+   "neighborhood": "Pleasant Valley ",
+   "housing size": "2-BR",
+   "rent": "990"
+ },
+ {
+   "neighborhood": "Pleasant Valley ",
+   "housing size": "3-BR",
+   "rent": "1244"
+ },
+ {
+   "neighborhood": "Pleasant Valley ",
+   "housing size": "Homeownership",
+   "rent": "1241"
+ },
+ {
+   "neighborhood": "Raleigh Hills",
+   "housing size": "Studio ",
+   "rent": "950"
+ },
+ {
+   "neighborhood": "Raleigh Hills",
+   "housing size": "1-BR",
+   "rent": "1012"
+ },
+ {
+   "neighborhood": "Raleigh Hills",
+   "housing size": "2-BR",
+   "rent": "1170"
+ },
+ {
+   "neighborhood": "Raleigh Hills",
+   "housing size": "3-BR",
+   "rent": "1389"
+ },
+ {
+   "neighborhood": "Raleigh Hills",
+   "housing size": "Homeownership",
+   "rent": "2116"
+ },
+ {
+   "neighborhood": "Roseway-Cully",
+   "housing size": "Studio ",
+   "rent": "680"
+ },
+ {
+   "neighborhood": "Roseway-Cully",
+   "housing size": "1-BR",
+   "rent": "847"
+ },
+ {
+   "neighborhood": "Roseway-Cully",
+   "housing size": "2-BR",
+   "rent": "1054"
+ },
+ {
+   "neighborhood": "Roseway-Cully",
+   "housing size": "3-BR",
+   "rent": "1175"
+ },
+ {
+   "neighborhood": "Roseway-Cully",
+   "housing size": "Homeownership",
+   "rent": "1434"
+ },
+ {
+   "neighborhood": "Sellwood-Moreland-Brooklyn",
+   "housing size": "Studio ",
+   "rent": "1159"
+ },
+ {
+   "neighborhood": "Sellwood-Moreland-Brooklyn",
+   "housing size": "1-BR",
+   "rent": "1035"
+ },
+ {
+   "neighborhood": "Sellwood-Moreland-Brooklyn",
+   "housing size": "2-BR",
+   "rent": "1408"
+ },
+ {
+   "neighborhood": "Sellwood-Moreland-Brooklyn",
+   "housing size": "3-BR",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Sellwood-Moreland-Brooklyn",
+   "housing size": "Homeownership",
+   "rent": "1963"
+ },
+ {
+   "neighborhood": "South Portland-Maraquam Hill",
+   "housing size": "Studio ",
+   "rent": "1332"
+ },
+ {
+   "neighborhood": "South Portland-Maraquam Hill",
+   "housing size": "1-BR",
+   "rent": "1405"
+ },
+ {
+   "neighborhood": "South Portland-Maraquam Hill",
+   "housing size": "2-BR",
+   "rent": "2116"
+ },
+ {
+   "neighborhood": "South Portland-Maraquam Hill",
+   "housing size": "3-BR",
+   "rent": "1462"
+ },
+ {
+   "neighborhood": "South Portland-Maraquam Hill",
+   "housing size": "Homeownership",
+   "rent": "1918"
+ },
+ {
+   "neighborhood": "St. Johns",
+   "housing size": "Studio ",
+   "rent": "1156"
+ },
+ {
+   "neighborhood": "St. Johns",
+   "housing size": "1-BR",
+   "rent": "871"
+ },
+ {
+   "neighborhood": "St. Johns",
+   "housing size": "2-BR",
+   "rent": "955"
+ },
+ {
+   "neighborhood": "St. Johns",
+   "housing size": "3-BR",
+   "rent": "1208"
+ },
+ {
+   "neighborhood": "St. Johns",
+   "housing size": "Homeownership",
+   "rent": "1241"
+ },
+ {
+   "neighborhood": "Tryon Creek-South Terwilliger",
+   "housing size": "Studio ",
+   "rent": "849"
+ },
+ {
+   "neighborhood": "Tryon Creek-South Terwilliger",
+   "housing size": "1-BR",
+   "rent": "1046"
+ },
+ {
+   "neighborhood": "Tryon Creek-South Terwilliger",
+   "housing size": "2-BR",
+   "rent": "1279"
+ },
+ {
+   "neighborhood": "Tryon Creek-South Terwilliger",
+   "housing size": "3-BR",
+   "rent": "-"
+ },
+ {
+   "neighborhood": "Tryon Creek-South Terwilliger",
+   "housing size": "Homeownership",
+   "rent": "2120"
+ },
+ {
+   "neighborhood": "West Portland",
+   "housing size": "Studio ",
+   "rent": "923"
+ },
+ {
+   "neighborhood": "West Portland",
+   "housing size": "1-BR",
+   "rent": "1066"
+ },
+ {
+   "neighborhood": "West Portland",
+   "housing size": "2-BR",
+   "rent": "1381"
+ },
+ {
+   "neighborhood": "West Portland",
+   "housing size": "3-BR",
+   "rent": "1672"
+ },
+ {
+   "neighborhood": "West Portland",
+   "housing size": "Homeownership",
+   "rent": "1699"
+ },
+ {
+   "neighborhood": "Woodstock",
+   "housing size": "Studio ",
+   "rent": "927"
+ },
+ {
+   "neighborhood": "Woodstock",
+   "housing size": "1-BR",
+   "rent": "1014"
+ },
+ {
+   "neighborhood": "Woodstock",
+   "housing size": "2-BR",
+   "rent": "1261"
+ },
+ {
+   "neighborhood": "Woodstock",
+   "housing size": "3-BR",
+   "rent": "1314"
+ },
+ {
+   "neighborhood": "Woodstock",
+   "housing size": "Homeownership",
+   "rent": "1903"
+ }
+]
